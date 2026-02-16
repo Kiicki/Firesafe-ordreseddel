9a6cc443b3b92f975259e4be7356d33f9d9c8ad7
Kiicki <kulasic.igor@gmail.com>
2026-02-16 19:40:06 +0100

    asdasd

--- script-ui.js
+++ script-ui.js
@@ -1680,13 +1680,13 @@ async function renderSettingsTemplateList() {
                 (detail ? '<div class="settings-template-item-row2">' + detail + '</div>' : '') +
             '</div>' +
             '<div class="settings-template-item-actions">' +
-                '<button class="settings-template-delete-btn" onclick="event.stopPropagation(); deleteTemplateFromSettings(\'' + id + '\')" title="' + t('delete_btn') + '">' +
-                    '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 6h18M8 6V4a2 2 0 012-2h4a2 2 0 012 2v2m3 0v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6h14"/></svg>' +
-                '</button>' +
                 '<label class="settings-toggle" onclick="event.stopPropagation()">' +
                     '<input type="checkbox"' + (isActive ? ' checked' : '') + ' onchange="toggleTemplateActive(\'' + id + '\')">' +
                     '<span class="settings-toggle-slider"></span>' +
                 '</label>' +
+                '<button class="settings-template-delete-btn" onclick="event.stopPropagation(); deleteTemplateFromSettings(\'' + id + '\')" title="' + t('delete_btn') + '">' +
+                    deleteIcon +
+                '</button>' +
             '</div>' +
         '</div>';
     }).join('');
